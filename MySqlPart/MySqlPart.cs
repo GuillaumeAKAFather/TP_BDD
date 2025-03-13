28addc054e33f00712490ef885f50171b5df0f0d
Excille <texcille@gmail.com>
2025-03-13 14:34:44 +0100

    Push

--- MySqlPart/MySqlPart.cs
+++ MySqlPart/MySqlPart.cs
@@ -5,50 +5,124 @@ using MySql.Data.MySqlClient;
 public class MySqlPart{
 
     MySqlConnection connection;
-    MySqlCommand command;
     MySqlDataReader reader;
 
 
     public MySqlPart(){
-        string connectString = "server=localhost;uid=root;pwd=qkvdig;database=tb_bdd";
+        string connectString = "server=81.1.20.23;uid=EtudiantJvd;pwd='!?CnamNAQ01?!';database=USRS6N_1;port=3306";
         connection = new MySqlConnection();
         connection.ConnectionString = connectString;
         connection.Open();
+
     }
+
+    
 
 
     public void HandleSubscription(){
+
+        string player_pseudo = string.Empty;
         string player_name = string.Empty;
-        string player_age = string.Empty;
+        string player_lastName = string.Empty;
+        int player_age = 0;
         string player_adress = string.Empty;
+        string player_pswd=  string.Empty;
 
-        while(player_name == string.Empty){
-            Console.WriteLine("Enter your name");
-            player_name = Console.ReadLine();
+
+        
+        Console.WriteLine("Enter your pseudo : ");
+        player_pseudo = Console.ReadLine();
+        
+        Console.WriteLine("Enter a password : ");
+        player_pswd = Console.ReadLine();
+
+        Console.WriteLine("Enter your first name : ");
+        player_name = Console.ReadLine();
+
+        Console.WriteLine("Enter your last name : ");
+        player_lastName = Console.ReadLine();
+
+        while(true){
+            Console.WriteLine("Enter your age : ");
+            string input = Console.ReadLine();
+            if(int.TryParse(input, out int result)){
+                break;
+            }
+            player_age = result;
         }
 
-        while(player_age == string.Empty){
-            Console.WriteLine("Enter your age");
-            player_age = Console.ReadLine();
+        Console.WriteLine("Enter your mail : ");
+        player_adress = Console.ReadLine();
 
+        
+
+        string sql = $"insert into Gr6_Joueurs ( pseudo, motDePasse, prenom, nom, email, age) VALUES ('{player_pseudo}', '{player_pswd}', '{player_name}' , '{player_lastName}', '{player_adress}' , '{player_age}');";
+        ExecuteSql(sql);
+
+        Console.WriteLine("Subscription done !");
+    }
+
+
+
+    public int PseudoID(string pseudoEntered){
+        string sql = $"select joueurId from Gr6_Joueurs where pseudo='{pseudoEntered}'";
+        using (MySqlDataReader result = GetSqlResults(sql)){
+                if(result.Read()){
+                return result.GetInt32(0);
+            }
+        }
+        return -1;
+        
+    }
+    public bool CheckPassword(string password, int playerID){
+        string sql = $"select joueurId from Gr6_Joueurs where motDePasse='{password}'";
+        using(MySqlDataReader result = GetSqlResults(sql)){
+            while(result.Read()){
+                var res = (int)result["joueurID"];
+                if (res == playerID){
+                    return true;
+                }
+            }
         }
 
-        while(player_adress == string.Empty){
-            Console.WriteLine("Enter your adress");
-            player_adress = Console.ReadLine();
+        return false;
+    }
+
+    public List<int> JoinablePartieList(){
+        List<int> partyIds = new List<int>();
+        string sql = "select partieID from Gr6_Partie where etat='en cours'";
+        using(MySqlDataReader result = GetSqlResults(sql)){
+            while(result.Read()){
+                var res = (int)result["partieID"];
+                partyIds.Add(res);
+            }
         }
 
-        string sql = $"INSERT INTO tb_bdd.player_data ( player_name, player_age, player_adress) VALUES ('{player_name}', {player_age}, '{player_adress}');";
+        
+        return partyIds;
+    }
 
-        command = new MySqlCommand(sql, connection);
+    public void CreateGame(){
+        string sql = "insert into Gr6_Partie (dimensionCube, duree, etat, jeuId) values (3,60,'en cours',3)";
+        using (MySqlDataReader result = GetSqlResults(sql)){
+            Console.WriteLine("Game created");
+        }
+        
+    }
+
+    public void JoinGame(int gameIdToJoin){
+        
+    }
+
+    private MySqlDataReader GetSqlResults(string sql){
+        MySqlCommand command = new MySqlCommand(sql, connection);
+        return command.ExecuteReader();
+    }
+
+    private void ExecuteSql(string sql){
+        MySqlCommand command = new MySqlCommand(sql, connection);
         command.ExecuteReader();
     }
 
-    public void CreateParty(){
-        string sql = $"INSERT INTO tb_bdd.game_data (game_size) VALUES (7);";
-        command = new MySqlCommand(sql, connection);
-       
-        
-    }
 
 }
--- PreGamePart/PreGamePart.cs
+++ PreGamePart/PreGamePart.cs
@@ -1,29 +1,122 @@
 
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.VisualBasic;
 
 public class PreGameLoop{
 
     private MySqlPart sqlPart;
+    private int actualPlayerID;
 
     public void MainLoop(){
-
+        Console.WriteLine("Connecting to server ...");
         sqlPart = new MySqlPart();
+        Console.WriteLine("Connection done !");
 
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.WriteLine("------------- Bienvenue dans le jeu -------------");
-        Console.ForegroundColor = ConsoleColor.White;
-        Console.WriteLine("1 - Se connecter");
-        Console.WriteLine("2 - S'inscrire");
+        Welcome();
 
-        var result = int.Parse(Console.ReadLine());
+        // Console.ForegroundColor = ConsoleColor.Yellow;
+        // Console.WriteLine("------------- Bienvenue dans le jeu -------------");
+        // Console.ForegroundColor = ConsoleColor.White;
+        // Console.WriteLine("1 - Se connecter");
+        // Console.WriteLine("2 - S'inscrire");
 
-        if(result == 1){
-            sqlPart.HandleSubscription();
-        }
+        // var result = int.Parse(Console.ReadLine());
+
+        // if(result == 1){
+        //     sqlPart.HandleSubscription();
+        // }
 
 
     }   
 
+    private void Welcome(){
+        Console.WriteLine("----------------------Bienvenue dans ce putain de jeu----------------------");
+        
+        int result = -1;
+        while(true){
+            Console.WriteLine("1 - S'inscrire");
+            Console.WriteLine("2 - Se connecter");
 
+            string input = Console.ReadLine();
+            if(int.TryParse(input, out result) && result <= 2 && result >= 1){
+                break;
+            }
+
+        }
+        switch(result){
+            case 1 :
+            sqlPart.HandleSubscription();
+            Welcome();
+            break;
+            case 2 :
+            ConnectToTheGame();
+            break;
+        }
+    }
+
+    private void ConnectToTheGame(){
+        Console.WriteLine("Enter your pseudo :");
+        string? result = Console.ReadLine();
+        int pseudoID = sqlPart.PseudoID(result);
+        
+        if(pseudoID>-1){
+            //Console.WriteLine("Pseudo exists "+ pseudoID);
+            Console.WriteLine("Enter your password : ");
+            bool goodPasswordEntered = sqlPart.CheckPassword(Console.ReadLine(), pseudoID);
+            if(goodPasswordEntered){
+                Console.WriteLine("Connected ! ");
+                SelectGameState();
+
+            }else{
+                Console.WriteLine("Password incorrect.");
+                Welcome();
+            }
+        }
+        else{
+            Console.WriteLine("Pseudo does not exists");
+            Welcome();
+            
+        }
+
+    }
+
+    private void SelectGameState(){
+        int result = 0;
+        while(true){
+            Console.WriteLine("1 - Create game");
+            Console.WriteLine("2 - Join game");
+            Console.WriteLine("3 - Print joinable game");
+            string input = Console.ReadLine();
+            if(int.TryParse(input, out result) && result >= 1 && result <= 3){
+                break;
+            }
+        }
+
+        switch(result){
+            case 1: //Create game
+            sqlPart.CreateGame();
+            SelectGameState();
+            break;
+            case 2:// Join game
+
+            while(true){
+            Console.WriteLine("Enter the game id you want to join : ");
+            string input = Console.ReadLine();
+            if(int.TryParse(input, out int gameIdShoo)){
+                break;
+            }
+
+            
+        }
+            break;
+            case 3 : // Print joinable game
+            List<int> joinableListIds = sqlPart.JoinablePartieList();
+            foreach(int id in joinableListIds){
+                Console.WriteLine("Joinable game with id : " + id);
+            }
+            SelectGameState();
+            break;
+        }
+    }
 
 }
